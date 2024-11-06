6c2475fe8001d51dfb267fb7d1b42e20472d1eb6
A-D-Fuchs <alexander.d.fuchs@fau.de>
2024-11-06 14:04:56 +0100

    added files

--- src/nomad_parser_plugins_camels_files/parsers/__init__.py
+++ src/nomad_parser_plugins_camels_files/parsers/__init__.py
@@ -2,17 +2,18 @@ from nomad.config.models.plugins import ParserEntryPoint
 from pydantic import Field
 
 
-class NewParserEntryPoint(ParserEntryPoint):
+class CamelsParserEntryPoint(ParserEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from nomad_parser_plugins_camels_files.parsers.parser import NewParser
+        from nomad_parser_plugins_camels_files.parsers.parser import CamelsParser
 
-        return NewParser(**self.dict())
+        return CamelsParser(**self.dict())
 
 
-parser_entry_point = NewParserEntryPoint(
-    name='NewParser',
+camels_parser_entry_point = CamelsParserEntryPoint(
+    name='CamelsParser',
     description='New parser entry point configuration.',
-    mainfile_name_re='.*\.camels',
+    mainfile_name_re=r'^.*\.(h5|hdf5|nxs)$',
+    mainfile_mime_re='(application/x-hdf)',
 )
--- src/nomad_parser_plugins_camels_files/parsers/parser.py
+++ src/nomad_parser_plugins_camels_files/parsers/parser.py
@@ -11,15 +11,28 @@ if TYPE_CHECKING:
     )
 
 from nomad.config import config
-from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
+from nomad.datamodel.metainfo.basesections import Experiment
+from nomad.datamodel import EntryData
+from nomad.metainfo import Quantity
+
+from nomad.datamodel.hdf5 import HDF5Reference
+
+import os
 
 configuration = config.get_plugin_entry_point(
-    'nomad_parser_plugins_camels_files.parsers:parser_entry_point'
+    'nomad_parser_plugins_camels_files.parsers:camels_parser_entry_point'
 )
 
+class CamelsData(Experiment, EntryData):
+    quantity = Quantity(
+        type=HDF5Reference,
+        description='test data reference',
+    )
 
-class NewParser(MatchingParser):
+
+
+class CamelsParser(MatchingParser):
     def parse(
         self,
         mainfile: str,
@@ -27,5 +40,12 @@ class NewParser(MatchingParser):
         logger: 'BoundLogger',
         child_archives: dict[str, 'EntryArchive'] = None,
     ) -> None:
-        logger.info('NewParser.parse', parameter=configuration.parameter)
-        
+        self.archive = archive
+        self._logger = logger if logger else get_logger(__name__)
+        *_, self._fname = mainfile.rsplit("/", 1)
+        data = CamelsData()
+        data.quantity = f'{os.path.basename(mainfile)}#/CAMELS_KAUST/data/Simple_Sweep/ElapsedTime'
+
+
+
+
