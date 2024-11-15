1204ebd0375212c235e33c058ce2faed23908836
A-D-Fuchs <alexander.d.fuchs@fau.de>
2024-11-15 12:15:44 +0100

    very simple app

--- src/nomad_parser_plugins_camels_files/apps/__init__.py
+++ src/nomad_parser_plugins_camels_files/apps/__init__.py
@@ -1,36 +1,23 @@
-# import yaml
-# from nomad.config.models.plugins import AppEntryPoint
-# from nomad.config.models.ui import (
-#     App,
-#     Column,
-#     Columns,
-#     FilterMenu,
-#     FilterMenus,
-#     Filters,
-# )
+import yaml
+from nomad.config.models.plugins import AppEntryPoint
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Columns,
+    FilterMenu,
+    FilterMenus,
+    Filters,
+)
 
-# yaml_data = """
-#         label: My App
-#         path: myapp
-#         category: Theory
-#         """
-# myapp = AppEntryPoint(
-#     name="CAMELSApp",
-#     description="App defined using the new plugin mechanism.",
-#     app=App(
-#         label="NewApp",
-#         path="app",
-#         category="simulation",
-#         columns=Columns(
-#             selected=["entry_id"],
-#             options={
-#                 "entry_id": Column(),
-#             },
-#         ),
-#         filter_menus=FilterMenus(
-#             options={
-#                 "material": FilterMenu(label="Material"),
-#             }
-#         ),
-#     ),
-# )
+yaml_data = """
+        label: My App
+        path: myapp
+        category: Theory
+        """
+camels_app = AppEntryPoint(
+    name="CAMELSApp",
+    description="App defined using the new plugin mechanism.",
+    app=App.parse_obj(
+            yaml.safe_load(yaml_data)
+    ),
+)
