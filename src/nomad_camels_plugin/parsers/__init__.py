16c3bd6d58caf45489f2b13abe4229bc854bc419
A-D-Fuchs <alexander.d.fuchs@fau.de>
2025-09-05 17:15:46 +0200

    now always add all available plots to the entries. added special CAMELS measurement entry for Diodes for conference

--- src/nomad_camels_plugin/parsers/__init__.py
+++ src/nomad_camels_plugin/parsers/__init__.py
@@ -1,5 +1,17 @@
 from nomad.config.models.plugins import ParserEntryPoint
 
+class CamelsParserDiodeEntryPoint(ParserEntryPoint):
+    def load(self):
+        from nomad_camels_plugin.parsers.parser import CamelsParserDiode
+
+        return CamelsParserDiode(**self.dict())
+
+camelsDiode_parser = CamelsParserDiodeEntryPoint(
+    name='CamelsParserDiode',
+    description='New parser entry point configuration.',
+    mainfile_name_re=r'^.*\.(h5|hdf5|nxs)$',
+    mainfile_mime_re='(application/x-hdf)',
+)
 
 class CamelsParserEntryPoint(ParserEntryPoint):
     def load(self):
